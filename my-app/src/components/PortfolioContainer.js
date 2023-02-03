b358f5c473c65b9e8ef223cdae9a088477315fe9
Ryan Young <ryanbyoung1@gmail.com>
2023-02-03 17:26:58 -0500

    added component files and pages for each tab, also added in headshot image

---
+++ my-app/src/components/PortfolioContainer.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <About />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
+
---
+++ my-app/src/components/pages/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import image from './assets/headshot.jpeg';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <img src={image} alt='Image of Ryan Young'></img>
+      <p>
+        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      </p>
+    </div>
+  );
+}

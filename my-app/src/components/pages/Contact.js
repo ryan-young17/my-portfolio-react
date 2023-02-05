584e8386f0551cca768aa0e01ab71b02ab776d12
Ryan Young <ryanbyoung1@gmail.com>
2023-02-05 11:18:00 -0500

    added contact form and styled page

--- my-app/src/components/pages/Contact.js
+++ my-app/src/components/pages/Contact.js
@@ -3,20 +3,24 @@ import React from 'react';
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <h1 className='ms-5 mt-5 mb-3 display-3'>Contact</h1>
+      <div className='ms-5 me-5'>
+        <form>
+          <div class="mb-3 form-outline w-50">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="mb-3 form-outline w-50">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          </div>
+          <div class="mb-3 input-group-lg form-outline w-50">
+            <label class="form-label">Message</label>
+            <textarea type="text" class="form-control" />
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }

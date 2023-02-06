f8a5893bb84774d5a128fea867e849e268e57cc7
Ryan Young <ryanbyoung1@gmail.com>
2023-02-06 17:31:55 -0500

    edited resume page to make it mobile responsive

--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -5,15 +5,15 @@ import image from './assets/young_resume.png';
 export default function Resume() {
   return (
     <div className='container'>
-      <div className='row'>
-        <div className='col-3 mt-5 pt-5'>
+      <div className='row justify-content-center'>
+        <div className='col-12 col-md-4 col-lg-3 mt-5 mb-1'>
             <h4>Download Resume Below:</h4>
-            <button className='btn btn-primary ms-5 mt-2'>
+            <button className='btn btn-primary'>
               <a className='text-light text-decoration-none' href={pdf} download>Download Here</a>
             </button>
         </div>
-        <div className='col-9 mt-5 pt-4 mb-5'>
-          <img className='border border-2 border-primary' src={image} width='850px' height='1100px' alt='Image of resume'></img>
+        <div className='col-12 col-md-8 col-lg-9 mt-5 mb-5'>
+          <img className='border border-2 border-primary img-fluid' src={image} width='850px' height='1100px' alt='Image of resume'></img>
         </div>
       </div>
     </div>

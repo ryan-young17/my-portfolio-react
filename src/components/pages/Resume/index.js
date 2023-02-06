448711bf41c6139cf80a1347f2ec8c75184f225a
Ryan Young <ryanbyoung1@gmail.com>
2023-02-06 16:34:04 -0500

    edited styling on resume page

--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -4,15 +4,17 @@ import image from './assets/young_resume.png';
 
 export default function Resume() {
   return (
-    <div>
-      <h1 className='mt-5 mb-3  d-flex justify-content-center display-3'>Resume</h1>
-      <div className='d-flex justify-content-center'>
-        <button className='btn btn-primary mb-2'>
-          <a className='text-light text-decoration-none' href={pdf} download>Download Here</a>
-        </button>
-      </div>
-      <div className='d-flex justify-content-center mt-4 mb-5'>
-        <img className='border border-2 border-primary' src={image} width='850px' height='1100px' alt='Image of resume'></img>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-3 mt-5 pt-5'>
+            <h4>Download Resume Below:</h4>
+            <button className='btn btn-primary ms-5 mt-2'>
+              <a className='text-light text-decoration-none' href={pdf} download>Download Here</a>
+            </button>
+        </div>
+        <div className='col-9 mt-5 pt-4 mb-5'>
+          <img className='border border-2 border-primary' src={image} width='850px' height='1100px' alt='Image of resume'></img>
+        </div>
       </div>
     </div>
   );

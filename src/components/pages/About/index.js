b44bab425117ecf83bcc281d67323dc34166d5b7
Ryan Young <ryanbyoung1@gmail.com>
2023-02-06 17:24:46 -0500

    edited layout on about page

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -11,20 +11,22 @@ const styles = {
 export default function About() {
   return (
     <div>
-      <h1 className='mt-5 mb-3 d-flex justify-content-center display-3'>About Me</h1>
-      <div className='row m-5'>
-        <div className='col-3'>
-          <img style={styles.image} src={image} alt='Image of Ryan Young'></img>
-        </div>
-        <div className='col-9 p-5'>
-          <p>
-            Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-            Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-            mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-            lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-            imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-            in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-          </p>
+      <h1 className='m-5 d-flex justify-content-center display-3'>About Me</h1>
+      <div className='container'>
+        <div className='row justify-content-center m-3'>
+          <div className='col-12 col-md-4 col-lg-3 mb-3'>
+            <img className="img-fluid" style={styles.image} src={image} alt='Image of Ryan Young'></img>
+          </div>
+          <div className='col-12 col-md-8 col-lg-9'>
+            <p>
+              Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+              Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+              mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+              lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+              imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+              in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+            </p>
+          </div>
         </div>
       </div>
     </div>
